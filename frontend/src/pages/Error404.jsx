1cc689013fc7e4d608a5008c2a9d99a1888b382b
Moaz Abdullah <moazabdullah02@gmail.com>
2024-08-21 12:36:05 +0500

    Initial commit

--- frontend/src/pages/Error404.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Error404() {
-  return (
-    <div className="flex items-center justify-center min-h-[70vh]">
-      <h1 className="text-4xl">Error 404! Page Not Found!</h1>
-    </div>
-  )
-}
