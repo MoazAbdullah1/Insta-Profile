3a988f9d9126ef1a7375015122bd517ed2a335a9
Moaz Abdullah <moazabdullah02@gmail.com>
2024-08-21 13:54:58 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import stories from './routes/stories.js'
 
 const app = express();
 app.use(cors())
-const port = 4000;
+const port = 5000;
 
 app.use(express.json())
 app.use("/followers", followers)
